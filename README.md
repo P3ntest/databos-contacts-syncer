5177c49b0fb6b4358bb1148fb4725db81c718acd
Julius van Voorden <julius.vanvoorden@savvytec.de>
2025-06-03 17:53:00 +0200

    finish

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# databos-contacts
+# DataBos Contacts Syncer
 
-To install dependencies:
+This project is a simple script to sync contacts from [DataBos](https://www.databos.de/) to a CardDAV server.
 
-```bash
-bun install
+It uses a reverse engineered DataBos API to fetch contacts and then uploads them to a CardDAV server using the `tsdav` package.
+
+# How to use
+
+Copy .env.example to .env and fill in all fields.
+
+## Using Docker (recommended)
+
+Build and run the Docker image (Dockerfile)
+
+## Native
+
+Install bun and then run `bun run start`
+
+# Environment variables
+
 ```
+DAV_URL= # URL of the CardDAV server, e.g. https://carddav.example.com
+DAV_USERNAME= # Username for the CardDAV server
+DAV_PASSWORD= # Password for the CardDAV server
 
-To run:
+DATABOS_URL= # The url of your DataBos instance, e.g. https://www.databos.de
+DATABOS_USERNAME= # Username for DataBos (can be a number)
+DATABOS_PASSWORD= # Password for DataBos
 
-```bash
-bun run index.ts
+CONTACT_ORGANIZATION= # This will be set as the organization for all contacts, e.g. "My Company"
 ```
-
-This project was created using `bun init` in bun v1.2.5. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import vCardsJS from "vcards-js";
 import { createDAVClient } from "tsdav";
 import { dataBos } from "./databos";
+import VCard from "vcard-creator";
 
 console.log("Hello via Bun!");
 
@@ -27,9 +27,14 @@ async function sync() {
 
   console.log("Using Address Book:", addressBook.displayName);
 
-  const vcards = await client.fetchVCards({
-    addressBook: addressBook,
-  });
+  const vcards = (
+    await client.fetchVCards({
+      addressBook: addressBook,
+    })
+  ).map((vcard) => ({
+    ...vcard,
+    uid: vcard.url.split("/").pop().replace(".vcf", ""),
+  }));
 
   console.log(`Found ${vcards.length} vCards in the address book.`);
 
@@ -47,49 +52,54 @@ async function sync() {
   const contacts = await dataBos.getContacts(sessionToken);
   console.log(`Found ${contacts.length} contacts in DataBos.`);
 
+  let i = 0;
   for (const contact of contacts) {
+    i++;
+    console.log(`Processing contact ${i}/${contacts.length}: ${contact.name}`);
     console.log(`Processing contact: ${contact.name}`);
 
     const fullContact = await dataBos.getContact(sessionToken, contact.id);
 
-    const card = vCardsJS();
-    card.version = "4.0";
-    card.firstName = contact.firstname;
-    card.lastName = contact.lastname;
-    // card.organization = Bun.env.CONTACT_ORGANIZATION;
-    card.uid = " hello world";
+    const card = new VCard();
+    card.addName(fullContact.name);
+    // card.addFirstName(contact.firstname);
+    // card.addLastName(contact.lastname);
+    card.addCompany(Bun.env.CONTACT_ORGANIZATION);
+    // card.addOrganization(Bun.env.CONTACT_ORGANIZATION, []);
+    card.addUID(contact.id.toString());
+    // card.setUID(contact.id.toString());
+
     // card.photo.attachFromUrl(fullContact.avatar, "JPEG");
 
-    // both phones
-    // card.cellPhone = fullContact.mobile;
-    // card.homePhone = fullContact.homephone;
-    // card.workPhone = fullContact.officephone;
+    fullContact.mobile && card.addPhoneNumber(fullContact.mobile, "CELL");
+    fullContact.homephone && card.addPhoneNumber(fullContact.homephone, "HOME");
+    fullContact.officephone &&
+      card.addPhoneNumber(fullContact.officephone, "WORK");
 
-    // card.email = fullContact.ffoMail || fullContact.email;
+    fullContact.email && card.addEmail(fullContact.email);
+    fullContact.ffoMail && card.addEmail(fullContact.ffoMail);
 
-    // card.homeAddress = {
-    //   street: fullContact.street,
-    //   city: fullContact.city,
-    //   postalCode: fullContact.zipcode.toString(),
-    //   label: "Home",
-    //   stateProvince: fullContact.appartment,
-    //   countryRegion: "Germany",
-    // };
-
-    const cardString = card.getFormattedString();
+    const cardString = card.toString();
     const vCardName = `${contact.id}.vcf`;
 
     console.log(`Creating vCard for ${contact.name} with ID ${contact.id}`);
     console.log("vCard content:", cardString);
     console.log("vCard filename:", vCardName);
+
+    const existingVCard = vcards.find((v) => v.uid === contact.id.toString());
+    if (existingVCard) {
+      console.log(`Deleting existing vCard for ${contact.name}`);
+      await client.deleteVCard({
+        vCard: vcards.find((v) => v.uid === contact.id.toString()),
+      });
+    }
+
     const res = await client.createVCard({
       addressBook: addressBook,
       vCardString: cardString,
       filename: vCardName,
     });
     console.log(`Created vCard for ${contact.name}`, await res.text());
-
-    break;
   }
 }
 
